b93892f104c11a3d16471f35c0c56e2f333942ee
Yael Bilu Eran <yael-be@yit.co.il>
2018-12-16 18:20:57 +0200

    first commit

---
+++ TaboolaTest_ios/ComponentCellView.swift
@@ -0,0 +1,24 @@
+//
+//  ComponentCellView.swift
+//  TaboolaTest_ios
+//
+//  Created by Yael Bilu Eran on 16/12/2018.
+//  Copyright © 2018 yit. All rights reserved.
+//
+
+import UIKit
+
+class ComponentCellView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TaboolaTest_ios/TaboolaComponentCellView.swift
@@ -0,0 +1,27 @@
+//
+//  Component.swift
+//  TaboolaTest_ios
+//
+//  Created by Yael Bilu Eran on 12/12/2018.
+//  Copyright © 2018 yit. All rights reserved.
+//
+
+import UIKit
+import TaboolaSDK
+
+class Component: UITableViewCell {
+
+    @IBOutlet weak var taboolaView: TaboolaView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+
